f8a6bdff3c7eaae4c2cd056b923329c11a6b9420
ibukunoluwanap <promisenap2001@gmail.com>
2023-02-25 13:33:11 +0200

    first init

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
+import { useEffect } from "react";
 import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css' // add bootstrap css
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return <Component {...pageProps} />
 }
