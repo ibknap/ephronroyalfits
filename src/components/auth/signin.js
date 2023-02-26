5ffd2f81b9a6c922654255abadbfb0aa2ac4beea
ibukunoluwanap <promisenap2001@gmail.com>
2023-02-26 20:23:20 +0200

    added auth ui

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,36 +1,44 @@
-import styles from '@/components/help_center/HelpCenter.module.css'
-import { Document } from 'iconsax-react';
+import styles from '@/components/auth/Auth.module.css'
 import Link from 'next/link';
 
 export default function Signin() {
     return (
         <>
-            <div className="d-flex h-100 text-center text-white bg-dark">
+            <div className={styles.container}>
                 <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
                     <header className="mb-auto">
-                        <div>
-                            <h3 className="float-md-start mb-0">Cover</h3>
-                            <nav className="nav nav-masthead justify-content-center float-md-end">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
-                                <a className="nav-link" href="#">Features</a>
-                                <a className="nav-link" href="#">Contact</a>
-                            </nav>
-                        </div>
+                        <h4 className="float-md-start mb-0">
+                            <Link className="text-decoration-none primary" href="/">
+                                NEFB
+                            </Link>
+                        </h4>
                     </header>
 
-                    <main className="px-3">
+                    <div className="px-3 row justify-content-center">
                         <h2>Sign In</h2>
                         <p>What&apos;s your email address</p>
                         <p className="text-muted">Type your email address to login to your account</p>
-                        <p className="lead">
-                            <a href="#" className="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
-                        </p>
-                    </main>
 
-                    <footer className="mt-auto text-white-50">
-                        <p>
-                            For further support, you may visit the <Link href="/help_center" className="text-decoration-none primary">Help Center.</Link>
-                        </p>
+                        <form className="col-md-4 mt-4">
+                            <div className="form-floating mb-3">
+                                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                                <label for="floatingInput">Email address</label>
+                            </div>
+                            <div className="form-floating">
+                                <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                                <label for="floatingPassword">Password</label>
+                            </div>
+                            <button className={`btn btn-lg btn-success ${styles.auth_btn} col-md-8 mt-4`}>Signin</button>
+                            <p className="mt-4">
+                                Want to do something different? Reset your password
+                                <Link href="/auth/reset_password" className="text-decoration-none primary"> here</Link> or 
+                                <Link href="/auth/signup" className="text-decoration-none primary"> signup</Link> a new account.
+                            </p>
+                        </form>
+                    </div>
+
+                    <footer className="mt-auto text-muted">
+                        For further support, you may visit the <Link href="/help_center" className="text-decoration-none primary">Help Center.</Link>
                     </footer>
                 </div>
             </div>
--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,7 +1,46 @@
+import styles from '@/components/auth/Auth.module.css'
+import Link from 'next/link';
+
 export default function Signup() {
     return (
         <>
+            <div className={styles.container}>
+                <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+                    <header className="mb-auto">
+                        <h4 className="float-md-start mb-0">
+                            <Link className="text-decoration-none primary" href="/">
+                                NEFB
+                            </Link>
+                        </h4>
+                    </header>
 
+                    <div className="px-3 row justify-content-center">
+                        <h2>Sign Up</h2>
+                        <p>Create an account to get started</p>
+                        <p className="text-muted">Input your details below to complete.</p>
+
+                        <form className="col-md-4 mt-4">
+                            <div className="form-floating mb-3">
+                                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                                <label for="floatingInput">Email address</label>
+                            </div>
+                            <div className="form-floating">
+                                <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                                <label for="floatingPassword">Password</label>
+                            </div>
+                            <button className={`btn btn-lg btn-success ${styles.auth_btn} col-md-8 mt-4`}>Signup</button>
+                            <p className="mt-4">
+                                Have account already?
+                                <Link href="/auth/signin" className="text-decoration-none primary"> signin</Link>.
+                            </p>
+                        </form>
+                    </div>
+
+                    <footer className="mt-auto text-muted">
+                        For further support, you may visit the <Link href="/help_center" className="text-decoration-none primary">Help Center.</Link>
+                    </footer>
+                </div>
+            </div>
         </>
     )
 }
