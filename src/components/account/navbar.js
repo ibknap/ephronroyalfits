3baf9c6ff0896acbc3d205b2942fe399bb54e723
ibukunoluwanap <promisenap2001@gmail.com>
2023-12-09 17:26:39 +0200

    upd

--- src/components/account/navbar.js
+++ src/components/account/navbar.js
@@ -9,7 +9,7 @@ export default function AccountNavbar() {
   const [showMenu, setShowMenu] = useState(false);
   const [isMobile] = useMediaQuery("(max-width: 768px)");
   const router = useRouter();
-  const { logOut } = useAuth();
+  const { logOut, authUser } = useAuth();
 
   // open and collapse navbar depending on view port
   const showMenuToggler = () => setShowMenu(!showMenu);
@@ -28,6 +28,22 @@ export default function AccountNavbar() {
         <div className="col-md-3">
           <div className="m-2 p-2 shadow-sm">
             <ul className="list-unstyled">
+              {authUser && (
+                <li
+                  className={`p-2 ${
+                    router.asPath == "/dashboard/orders" && "bg_primary_50"
+                  }`}
+                >
+                  <Link
+                    className="text-dark text-decoration-none"
+                    href="/dashboard/orders"
+                  >
+                    <User className="me-2" variant="Bulk" />
+                    Dashboard
+                  </Link>
+                </li>
+              )}
+
               <li
                 className={`p-2 ${
                   router.asPath == "/account" && "bg_primary_50"
